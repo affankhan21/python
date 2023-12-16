88ef4b1ed06159d07ef58c481163b1be375df069
AFFAN SIRAJUDDIN KHAN <affankhan2108@gmail.com>
2023-12-16 21:00:30 +0530

    Add files via upload

---
+++ array.py
@@ -0,0 +1,7 @@
+import array
+data=array.array('i',[3,4,21,56,7])
+print(data)
+print(type(data))
+data.append(89)
+print(data)
+print(len(data))
---
+++ frozenset.py
@@ -0,0 +1,6 @@
+data=frozenset([45,67,34])
+print(data)
+for x in data:
+    print(x)
+print(len(data))    
+print(sorted(data))
